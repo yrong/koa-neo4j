79750d6dfed1c94c90da7ebb29f180458156dcce
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2016-10-05 19:46:39 +0330

    remember me option in auth WIP

--- src/auth.js
+++ src/auth.js
@@ -59,7 +59,7 @@ class Authentication {
                 user.roles = roles;
                 const options = {};
                 if (!ctx.request.body.remember)
-                    options.expiresIn = 30;
+                    options.expiresIn = 20;
                 ctx.body = {
                     token: `JWT ${jwt.sign(user, this.secret, options)}`,
                     user: user
